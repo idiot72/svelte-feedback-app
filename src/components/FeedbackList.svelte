d3d353e0b35591401e3ede6eb35603f7e98db075
idiot72 <vidurshah12@gmail.com>
2022-06-14 11:51:12 -0400

    a little funny

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,20 +1,7 @@
 <script>
-  // import {onMount, onDestroy} from 'svelte'
   import {FeedbackStore} from '../stores'
   import { fade, scale } from 'svelte/transition'
   import FeedbackItem from './FeedbackItem.svelte'
-
-  // let feedback = []
-
-  // const unsubscribe = FeedbackStore.subscribe((data) => feedback = data)
-
-  // onMount(() => {
-  //   console.log('mounted')
-  // })
-
-  // onDestroy(() => {
-  //  unsubscribe()
-  // })
 </script>
 
 {#each $FeedbackStore as fb (fb.id)}
--- src/stores.js
+++ src/stores.js
@@ -4,16 +4,16 @@ export const FeedbackStore = writable([
   {
     id: 1,
     rating: 10,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    text: 'Wow this is so nice.',
   },
   {
     id: 2,
-    rating: 9,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    rating: 1,
+    text: 'i actually hated it. would have been 0 stars if allowed.',
   },
   {
     id: 3,
     rating: 8,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    text: 'haha witty response.',
   },
 ])
